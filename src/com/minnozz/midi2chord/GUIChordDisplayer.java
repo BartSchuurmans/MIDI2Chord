defaab9428f22b20e0f733f16ebca64b6843f7a6
Bart Schuurmans <bart@minnozz.com>
2011-12-26 22:09:24 +0100

    GUIChordDisplayer: Display up to 4 alternatives

--- src/com/minnozz/midi2chord/GUIChordDisplayer.java
+++ src/com/minnozz/midi2chord/GUIChordDisplayer.java
@@ -6,6 +6,9 @@ import javax.swing.*;
 
 public class GUIChordDisplayer extends ChordDisplayer {
 	private JLabel bigChord;
+	private JLabel alternative1;
+	private JLabel alternative2;
+	private JLabel alternative3;
 
 	@Override
 	public void run() {
@@ -14,20 +17,50 @@ public class GUIChordDisplayer extends ChordDisplayer {
 
 		Container contentPane = frame.getContentPane();
 		GroupLayout layout = new GroupLayout(contentPane);
-		layout.setAutoCreateContainerGaps(true);
 		contentPane.setLayout(layout);
 
+		// Create fonts
+		Font bigFont = new Font(Font.SANS_SERIF, Font.BOLD, 96);
+		Font mediumFont = new Font(Font.SANS_SERIF, Font.BOLD, 32);
+
 		// Create display areas
-		bigChord = new JLabel("-", SwingConstants.CENTER);
-		bigChord.setSize(200, 200);
+		bigChord = new JLabel("-");
+		bigChord.setHorizontalAlignment(SwingConstants.CENTER);
+		bigChord.setMinimumSize(new Dimension(450, 240));
+		bigChord.setFont(bigFont);
+
+		alternative1 = new JLabel("-");
+		alternative1.setHorizontalAlignment(SwingConstants.CENTER);
+		alternative1.setMinimumSize(new Dimension(150, 80));
+		alternative1.setFont(mediumFont);
+
+		alternative2 = new JLabel("-");
+		alternative2.setHorizontalAlignment(SwingConstants.CENTER);
+		alternative2.setMinimumSize(new Dimension(150, 80));
+		alternative2.setFont(mediumFont);
+
+		alternative3 = new JLabel("-");
+		alternative3.setHorizontalAlignment(SwingConstants.CENTER);
+		alternative3.setMinimumSize(new Dimension(150, 80));
+		alternative3.setFont(mediumFont);
 
 		layout.setHorizontalGroup(
-			layout.createParallelGroup()
-			.addComponent(bigChord)
+			layout.createSequentialGroup()
+				.addComponent(bigChord)
+				.addGroup(layout.createParallelGroup()
+					.addComponent(alternative1)
+					.addComponent(alternative2)
+					.addComponent(alternative3)
+				)
 		);
 		layout.setVerticalGroup(
-			layout.createSequentialGroup()
-			.addComponent(bigChord)
+			layout.createParallelGroup()
+				.addComponent(bigChord)
+				.addGroup(layout.createSequentialGroup()
+					.addComponent(alternative1)
+					.addComponent(alternative2)
+					.addComponent(alternative3)
+				)
 		);
 
 		frame.pack();
@@ -40,12 +73,37 @@ public class GUIChordDisplayer extends ChordDisplayer {
 
 	@Override
 	public void display(ArrayList<Chord> options, ArrayList<Note> notes) {
-		if(options.isEmpty()) {
-			bigChord.setText("-");
-		} else {
-			for(Chord chord : options) {
-				bigChord.setText(chord.getName());
-			}
+		switch(options.size()) {
+			case 0:
+				bigChord.setText("-");
+				alternative1.setText("-");
+				alternative2.setText("-");
+				alternative3.setText("-");
+				break;
+			case 1:
+				bigChord.setText(options.get(0).getName());
+				alternative1.setText("-");
+				alternative2.setText("-");
+				alternative3.setText("-");
+				break;
+			case 2:
+				bigChord.setText(options.get(0).getName());
+				alternative1.setText(options.get(1).getName());
+				alternative2.setText("-");
+				alternative3.setText("-");
+				break;
+			case 3:
+				bigChord.setText(options.get(0).getName());
+				alternative1.setText(options.get(1).getName());
+				alternative2.setText(options.get(2).getName());
+				alternative3.setText("-");
+				break;
+			default:
+				bigChord.setText(options.get(0).getName());
+				alternative1.setText(options.get(1).getName());
+				alternative2.setText(options.get(2).getName());
+				alternative3.setText(options.get(3).getName());
+				break;
 		}
 	}
 }
